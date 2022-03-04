03ea507d161723c23a8d8ffc21632a5ccab73ced
Extermind <wpiotr952@gmail.com>
2022-03-04 22:52:39 +0100

    better input

---
+++ Dungeon/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject p;   // gameobject of player
+    private PlayerController player;
+    private Enemy enemy;
+    public GameState gs;
+
+    public enum GameState
+    {
+        DEFAULT, //should never happen
+        PLAYER_RESET,
+        PLAYER_TURN,
+        ENEMY_TURN,
+        ENEMY_RESET,
+        PLAYERS_DEATH,  //END OF GAME   
+        WIN             //win
+    }
+
+
+    private void Awake()
+    {
+        //p = GameObject.FindGameObjectWithTag("Player");
+        player = p.GetComponent<PlayerController>();
+        gs = GameState.PLAYER_RESET;
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gs)
+        {
+            case GameState.PLAYER_RESET:
+                player.resetActionPoints();
+                gs = GameState.PLAYER_TURN;
+                break;
+            case GameState.PLAYER_TURN:
+                player.turn();
+                if (player.PlayerState == PlayerController.PlayerStates.END_TURN)
+                {
+                    gs = GameState.ENEMY_RESET;
+                }
+                break;
+            case GameState.ENEMY_RESET:
+                gs = GameState.ENEMY_TURN;
+                break ;
+            case GameState.ENEMY_TURN:
+                //wait here 2 sec
+                Debug.Log("ENEMY TURN");
+                gs = GameState.PLAYER_RESET;
+                break;
+
+
+
+
+        }
+    }
+
+    void DoDelayAction(float delayTime)
+    {
+        StartCoroutine(DelayAction(delayTime));
+    }
+
+    IEnumerator DelayAction(float delayTime)
+    {
+        //Wait for the specified delay time before continuing.
+        yield return new WaitForSeconds(delayTime);
+
+        //Do the action after the delay time has finished.
+    }
+
+
+}
--- Dungeon/Assets/Scripts/Player/PlayerController.cs
+++ Dungeon/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,34 +9,98 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private int actionsPointsAmount = 30;
     [SerializeField]
-    private int actionsPoints;
+    private int actionsPoints = 0;
+
 
     [SerializeField]
     private int movementCostPoints = 5;
 
-
-
-    private PlayerActionsScript pa;
-    private Vector2 movement;
+    private Vector2 input;
+    [SerializeField]
+    private Vector3 direction;
+    
     private float moveSpeed = 5f;
     [SerializeField]
     private Transform movePoint;
 
+    public int pressindex = 0;
+
+
+    public PlayerStates PlayerState;
+    public enum PlayerStates
+    {
+        IDLE,
+        MOVING,
+        ATTACKING,
+        END_TURN
+    }
+
+    //Input Actions via Code
+    //"perfoming"
+    private InputAction chooseDirection;
+    //triggers
+    private InputAction movebtnAction;
+
+    void playerActions()
+    {
+        //setting up input
+
+        //choosing directions
+        chooseDirection = new InputAction("choseDirection");
+        //keyboard - WSAD
+        chooseDirection.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
+        //Left Stick
+        chooseDirection.AddBinding("<Gamepad>/leftStick");
+
+
+        //Interactions with objects
+
+        //move toward preferd direction
+        movebtnAction = new InputAction("movebtnAction",
+           type: InputActionType.Button,
+           binding: "<Keyboard>/space",
+           interactions: "press(behavior=1)");
+
+
+        //Enable all Actions in OnEnable() func      !!! also remenber to disable them in OnDisable() func
+        //chooseDirection.Enable();
+        //movebtnAction.Enable();
+
+
+        //get input only those that are preformed, for buttons we need to check in turn() func [triggers]
+        chooseDirection.performed += _ => input = _.ReadValue<Vector2>();
+       
+
+
+    }
+
+    void checkTriggers()
+    {
+        if (movebtnAction.triggered)
+        {
+            PlayerState = PlayerStates.MOVING;
+        }
+    }
+
     private void Awake()
     {
-        pa = new PlayerActionsScript();
 
-        //read input value
-        pa.player.movement.performed += x => movement = x.ReadValue<Vector2>();
-        pa.player.movement.canceled += x => movement = x.ReadValue<Vector2>();
+        playerActions();
+        
     }
     private void OnEnable()
     {
-        pa.Enable();
+        chooseDirection.Enable();
+        movebtnAction.Enable();
     }
     private void OnDisable()
     {
-        pa.Disable();
+        chooseDirection.Disable();
+        movebtnAction.Disable();
     }
 
 
@@ -43,31 +108,77 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         movePoint.parent = null;
-
-        actionsPoints = actionsPointsAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //
+    }
+
+    public void turn()
+    {
+        checkTriggers();
 
         //move player
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(input.x) == 1f)
+            {
+                direction = new Vector3((float)input.x, 0, 0);
+                //movePoint.position += new Vector3((float)movement.x, 0, 0);
+                //_actionsPoints -= movementCostPoints;
+            }
+            if (Mathf.Abs(input.y) == 1f)
+            {
+                direction = new Vector3(0,(float)input.y, 0);
+                //movePoint.position += new Vector3(0, (float)movement.y, 0);
+                //_actionsPoints -= movementCostPoints;
+            }
+
+            //choose new direction point and set it acordlingly xD?
+            if (actionsPoints >= movementCostPoints && PlayerState == PlayerStates.MOVING)
+            {
+                movePoint.position += direction;
+                PlayerState = PlayerStates.IDLE;
+            }
+
+            //check for "move points" if there are not enough change player state
+            if(actionsPoints < movementCostPoints && PlayerState == PlayerStates.IDLE)
+            {
+                PlayerState = PlayerStates.END_TURN;
+            }
 
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f && actionsPoints >= movementCostPoints)  
-        { 
-            if (Mathf.Abs(movement.x) == 1f)
-            {
-                movePoint.position += new Vector3((float)movement.x, 0, 0);
-                actionsPoints -= movementCostPoints;
-            }
-            if (Mathf.Abs(movement.y) == 1f)
-            {
-                movePoint.position += new Vector3(0, (float)movement.y, 0);
-                actionsPoints -= movementCostPoints;
-            }
         }
 
+        if (Vector3.Distance(transform.position, movePoint.position) == 1f)
+        {
+            actionsPoints -= movementCostPoints;
+        }
+    }
+
+
+    public void resetActionPoints()
+    {
+        actionsPoints = actionsPointsAmount;
+        PlayerState = PlayerStates.IDLE;
+    }
+    public int getActionPoints()
+    {
+        return actionsPoints; 
+    }
+    
+    public bool canMakeAction()
+    {
+        if(actionsPoints >= 5) 
+        { return true; }
+        else
+        {
+            return false;
+        }
     }
 }
